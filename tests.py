c0d85bdd7d413a07f05bc37962dd188201ab086a
willding123 <peijian.ding6@gmail.com>
2023-01-17 23:27:07 -0500

    update test.py

--- tests.py
+++ tests.py
@@ -129,40 +129,37 @@ class TestShift(unittest.TestCase):
         
 
 
-    # def test_model(self): 
-    #     x = torch.rand((4,3,224,224)).cuda()
-    #     x1 = torch.roll(x, (1,1), (2,3)).cuda()
-    #     label = torch.tensor((1,1,0,0)).cuda()
-    #     # model = SwinTransformer(3, (224,224), 1, qk_scale=1).cuda()
-    #     model1 = PolySwin(img_size=(224,224)).cuda()
-    #     criterion = torch.nn.CrossEntropyLoss()
-    #     print(model(x))
-    #     print(model1(x1))
-    #     torch.save(model(x), "tensor.pt")
-    #     torch.save(model1(x), "tensor1.pt")
+    def test_model(self): 
+        x = torch.rand((4,3,224,224)).cuda()
+        x1 = torch.roll(x, (1,1), (2,3)).cuda()
+        label = torch.tensor((1,1,0,0)).cuda()
+        # model = SwinTransformer(3, (224,224), 1, qk_scale=1).cuda()
+        model1 = PolySwin(img_size=(224,224)).cuda()
+        criterion = torch.nn.CrossEntropyLoss()
+        print(model(x))
+        print(model1(x1))
+        torch.save(model(x), "tensor.pt")
+        torch.save(model1(x), "tensor1.pt")
 
-    #     # for i in range(1):
-    #     #     s1 = torch.randint(5, (1,)); s2 = torch.randint(5, (1,)); 
-    #     #     x1 = torch.roll(x, (s1,s2), (2,3))
+        for i in range(1):
+            s1 = torch.randint(5, (1,)); s2 = torch.randint(5, (1,)); 
+            x1 = torch.roll(x, (s1,s2), (2,3))
             
-    #         # loss = criterion(output, label)
-    #         # loss1 = criterion(output1, label)
-    #         # assertAlmostEqual(loss, loss1, 5)
-
-    #         # print(output)
-    #         # print(output1)
-    #         # print(torch.linalg.norm(output-output1))
-    #         # print(f"loss: {loss}, loss1: {loss1}")
-            
-    #     # output1 = model(x1)
-    #     # loss = criterion(output, label)
-    #     # loss1 = criterion(output1, label)
-    #     # loss.backward()
-    #     # loss1.backward()
-
-    #     # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
-    #     # print(loss)
-    #     # print(loss1)
+            loss = criterion(output, label)
+            loss1 = criterion(output1, label)
+            assertAlmostEqual(loss, loss1, 5)
+            print(output)
+            print(output1)
+            print(torch.linalg.norm(output-output1))
+            print(f"loss: {loss}, loss1: {loss1}")
+        output1 = model(x1)
+        loss = criterion(output, label)
+        loss1 = criterion(output1, label)
+        loss.backward()
+        loss1.backward()
+        print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+        print(loss)
+        print(loss1)
 
 
         
@@ -195,25 +192,25 @@ class TestShift(unittest.TestCase):
         print(y)
         print(y1)
 
-    # def test_window_atten(self): 
-    #     x = torch.rand(64)
-    #     x = x.view(1,1, 8,8)
-    #     x[0,:,1::2, 0::2] = 1 
-    #     B,C,H,W = x.shape
-    #     x = x.view(B, H, W, C)
-    #     x = x.view(B, C, H, W)
-    #     xr = torch.roll(x, (1,1), (1,2))
-    #     x = poly_order(x, (2,2), (4,4))
-    #     xr =  poly_order(xr, (2,2), (4,4))
-    #     x = x.view(B, H, W, C)
-    #     xr = x.view(B, H, W, C)
+    def test_window_atten(self): 
+        x = torch.rand(64)
+        x = x.view(1,1, 8,8)
+        x[0,:,1::2, 0::2] = 1 
+        B,C,H,W = x.shape
+        x = x.view(B, H, W, C)
+        x = x.view(B, C, H, W)
+        xr = torch.roll(x, (1,1), (1,2))
+        x = poly_order(x, (2,2), (4,4))
+        xr =  poly_order(xr, (2,2), (4,4))
+        x = x.view(B, H, W, C)
+        xr = x.view(B, H, W, C)
 
-    #     print(x-xr)
-    #     x = x.view(B*4,-1, C)
-    #     xr = xr.view(B*4, -1 , C)
-    #     model = WindowAttention( 1, (4,4), 1)
-    #     print(model(x))
-    #     print(model(xr))
+        print(x-xr)
+        x = x.view(B*4,-1, C)
+        xr = xr.view(B*4, -1 , C)
+        model = WindowAttention( 1, (4,4), 1)
+        print(model(x))
+        print(model(xr))
 
 if __name__ == "__main__":
     test = TestShift()
