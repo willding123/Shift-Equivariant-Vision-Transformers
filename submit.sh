c862d42fad438b3a9233895fc049cbd07bd56891
willding123 <peijian.ding6@gmail.com>
2023-02-02 23:09:17 -0500

    update bash

--- submit.sh
+++ submit.sh
@@ -5,7 +5,7 @@
 #SBATCH --exclusive
 #SBATCH --gres=gpu:8
 #SBATCH --time=2-00:00:00  
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=pding@umd.edu
 
@@ -15,6 +15,4 @@ conda activate swin
 cd /fs/nexus-projects/shift-equivariant_vision_transformer/Swin-Transformer
 # batch size for a single gpu is 128
 nvidia-smi
-torchrun --nproc_per_node 8 --master_port 12345  main.py --cfg configs/swin/swin_tiny_patch4_window7_224_22k.yaml --data-path /fs/cml-datasets/ImageNet/ILSVRC2012 --batch-size 128 --output /fs/nexus-projects/shift-equivariant_vision_transformer --fused_window_process 
-
-# python main.py --cfg configs/swin/my_swin_tiny_patch4_window7_1k.yaml --data-path /fs/cml-datasets/ImageNet/ILSVRC2012 --batch-size 8 --output /fs/nexus-scratch/pding/output --fused_window_process 
+torchrun --nproc_per_node 8 --master_port 12345  main.py --cfg configs/swin/poly_swin_tiny_patch4_window7_1k_0202.yaml --data-path /fs/cml-datasets/ImageNet/ILSVRC2012 --batch-size 128 --output /fs/nexus-projects/shift-equivariant_vision_transformer --fused_window_process 
