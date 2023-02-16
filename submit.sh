ec4881fd1ce81bad79e3609fe1ec6a708c312e0e
willding123 <peijian.ding6@gmail.com>
2023-02-15 23:21:05 -0500

    update scripts for training vit

--- submit.sh
+++ submit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # bash script for single node gpu training 
-##SBATCH --account=scavenger
-##SBATCH --partition=scavenger
-##SBATCH --qos=scavenger
-#SBATCH --qos=high
-#SBATCH --exclusive
-#SBATCH --gres=gpu:4
-#SBATCH --time=1-00:00:00  
-#SBATCH --job-name=swin_0215
-#SBATCH --output=swin_0215.out
-#SBATCH --error=swin_0215.err
-#SBATCH --mem=64G
+#SBATCH --account=scavenger
+#SBATCH --partition=scavenger
+#SBATCH --qos=scavenger
+##SBATCH --qos=high
+##SBATCH --exclusive
+#SBATCH --gres=gpu:8
+#SBATCH --time=2-00:00:00  
+#SBATCH --job-name=vit_0215
+#SBATCH --output=vit_0215.out
+#SBATCH --error=vit_0215.err
+#SBATCH --mem=128G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=pding@umd.edu
 
@@ -20,4 +20,7 @@ source ~/miniconda3/bin/activate
 conda activate swin 
 cd /fs/nexus-projects/shift-equivariant_vision_transformer/Swin-Transformer
 # torchrun  --nproc_per_node 8  main.py --cfg configs/swin/poly_swin_tiny_0204.yaml --data-path /fs/cml-datasets/ImageNet/ILSVRC2012 --batch-size 128 --output /fs/nexus-projects/shift-equivariant_vision_transformer --fused_window_process 
-torchrun  --nproc_per_node 4  main.py --cfg configs/swin/poly_swin_tiny_0215.yaml --data-path /fs/cml-datasets/ImageNet/ILSVRC2012 --output /fs/nexus-projects/shift-equivariant_vision_transformer --fused_window_process 
+
+
+# ViT
+torchrun  --nproc_per_node 8  main.py --cfg configs/swin/poly_vit_tiny_0215.yaml --data-path /fs/cml-datasets/ImageNet/ILSVRC2012 --output /fs/nexus-projects/shift-equivariant_vision_transformer 
--- test_swin_block.py
+++ test_swin_block.py
@@ -271,12 +271,11 @@ confirm_bijective_matches_batch(t.cpu().detach().numpy(), t1.cpu().detach().nump
 #%% 
 ### test entire swin model 
 x = torch.rand((1,3,224,224)).cuda()
-num_test = 100 
-
+num_test = 1000
+model = PolySwin(img_size=img_size, norm_layer=nn.Identity).cuda()
+model.eval()
 for i in range(num_test):
-    model = PolySwin(img_size=img_size, norm_layer=nn.Identity).cuda()
-    model.eval()
-    shifts = tuple(np.random.randint(0,224,2))
+    shifts = tuple(np.random.randint(0,14,2))
     x1 = torch.roll(x, shifts, (2,3)).cuda()
     t = model(x)
     t1 = model(x1)
