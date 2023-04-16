e058febc1b9751be0a5c30cb9d0b4efa75104fd9
Peijian <peijian.ding6@gmail.com>
2023-04-15 20:49:42 -0400

    update analyze.py - tune power law param

--- analyze.py
+++ analyze.py
@@ -20,7 +20,7 @@ waitlist = []
 # directory where the log_rank files are located
 # dir_path = "/fs/nexus-scratch/pding/output/swin_tiny_patch4_window7_1k_default/default"
 # dir_path = "/fs/nexus-projects/shift-equivariant_vision_transformer/poly_swin_tiny_0215/default"
-dir_path = "/home/pding/scratch.cmsc663/pvit_small_w/default"
+dir_path = "/home/pding/scratch.cmsc663/twins_svts_w/default"
 i = 0
 # iterate through the files in the directory
 for file_name in os.listdir(dir_path):
@@ -50,19 +50,23 @@ ax.set_xscale('log', basex=10)
 plt.show()
 #%%
 # write a power law function
-def power_law(x, a, b):
-    return a * x ** b
+# write a power law function with three parameters a, b, and c
+def power_law(x, a, b, c):
+    return a * x ** b + c
 
 # fit the data using curve_fit
 from scipy.optimize import curve_fit
-popt, pcov = curve_fit(power_law, range(1, len(acc_ls)+1), acc_ls)
+if len(acc_ls) < 50: 
+    popt, pcov = curve_fit(power_law, range(1, len(acc_ls)+1), acc_ls)
+else:
+    popt, pcov = curve_fit(power_law, range(1, len(acc_ls)+1), acc_ls[:], maxfev=15000)
 
 # plot the data and the fitted function
 fig, ax = plt.subplots()
 ax.plot(range(1, len(acc_ls)+1), acc_ls, 'o', label='data')
 ax.plot(range(1, len(acc_ls)+1), power_law(range(1, len(acc_ls)+1), *popt), 'r-', label='fit')
-ax.set_xscale('log', basex=10)
-ax.set_yscale('log', basey=10)
+# ax.set_xscale('log', basex=10)
+# ax.set_yscale('log', basey=10)
 ax.legend()
 
 # display the plot
@@ -72,14 +76,14 @@ plt.show()
 fig, ax = plt.subplots()
 ax.plot(range(1, len(acc_ls)+1), acc_ls, 'o', label='data')
 ax.plot(range(1, len(acc_ls)+1), power_law(range(1, len(acc_ls)+1), *popt), 'r-', label='fit')
-ax.plot(range(1, 301), power_law(range(1, 301), *popt), 'g-', label='extrapolation')
-ax.set_xscale('log', basex=10)
-ax.set_yscale('log', basey=10)
+ax.plot(range(1, len(acc_ls)+50), power_law(range(1, len(acc_ls)+50), *popt), 'g-', label='extrapolation')
+# ax.set_xscale('log', basex=10)
+# ax.set_yscale('log', basey=10)
 ax.legend()
 plt.show()
 
 # extrapolate the data to n epochs: "extrapolate to n epochs:"
-n = 100
+n = 300
 print(f"extrapolate to {n} epochs: ", power_law(n, *popt))
 
 # %%
